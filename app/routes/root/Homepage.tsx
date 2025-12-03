e0761a0e3a80671f3f0d5b55f8a14779e1f50ec3
Lidetu Tadesse <lidetutadesse6@gmail.com>
2025-12-03 10:38:45 +0300

    comment removed

--- app/routes/root/Homepage.tsx
+++ app/routes/root/Homepage.tsx
@@ -294,20 +294,6 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
             </div>
             <div className="rounded-lg bg-white p-6 ring-8 ring-gray-900/5">
               <div className="mb-4 flex size-12 items-center justify-center rounded-xl bg-green-100">
-                {/* <svg
-          className="inline-block size-6 text-green-600"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-          ></path>
-        </svg> */}
                 <svg
                   className="inline-block size-6 text-green-600"
                   fill="none"
@@ -335,20 +321,6 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
             </div>
             <div className="rounded-lg bg-white p-6 ring-8 ring-gray-900/5">
               <div className="mb-4 flex size-12 items-center justify-center rounded-xl bg-purple-100">
-                {/* <svg
-          className="inline-block size-6 text-purple-600"
-          fill="none"
-          stroke="currentColor"
-          viewBox="0 0 24 24"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"
-          ></path>
-        </svg> */}
                 <svg
                   className="inline-block size-6 text-purple-600"
                   fill="none"
